6f90c67e31e2682b7ec5aeaa323237f99d824170
anand-reddi <kskreddy97@gmail.com>
2025-05-04 11:34:11 +0530

    made some changes..

--- android/app/src/main/java/app/lovable/c67f43a71d9c4832a0327b70a14483fb/MainActivity.java
+++ android/app/src/main/java/app/lovable/c67f43a71d9c4832a0327b70a14483fb/MainActivity.java
@@ -1,19 +1,5 @@
-
 package app.lovable.c67f43a71d9c4832a0327b70a14483fb;
 
-import android.os.Bundle;
-
 import com.getcapacitor.BridgeActivity;
-import com.capacitorjs.plugins.localnotifications.LocalNotificationsPlugin;
-import com.capacitorjs.plugins.pushnotifications.PushNotificationsPlugin;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        // Register plugins manually to ensure they're properly initialized
-        registerPlugin(LocalNotificationsPlugin.class);
-        registerPlugin(PushNotificationsPlugin.class);
-    }
-}
+public class MainActivity extends BridgeActivity {}
